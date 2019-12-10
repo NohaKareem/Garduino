d8085ed27dcf0e1eb526c8bf891242243adacca7
nobuko <smiledog@hotmail.com>
2019-12-10 17:28:47 -0500

    added electron

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,26 @@
+//Johnny-Five uses process.stdin, which is not available for Electron's use. 
+//This causes Electron's render process to crash! 
+//We can reroute process.stdin to a compatible replacement stream though
+
+//process.stdin is Node's way of getting data that's passed into it from outside. 
+//stdin , short for “standard in”, is the path by which we can pass data into an application. 
+//This is usually text data that the user has typed, but it can also come from another application.
+
+var Readable = require("stream").Readable;  
+var util = require("util");  
+util.inherits(MyStream, Readable);  
+function MyStream(opt) {  
+  Readable.call(this, opt);
+}
+MyStream.prototype._read = function() {};  
+// hook in our stream
+process.__defineGetter__("stdin", function() {  
+  if (process.__stdin) return process.__stdin;
+  process.__stdin = new MyStream();
+  return process.__stdin;
+});
+
+
 var vm = new Vue({ 
     el: "#app", 
     data: {
