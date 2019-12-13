0a08e20149c347f2e382d58702ea050e99b957c4
Noha Kareem <NohaKareem@users.noreply.github.com>
2019-12-13 00:38:51 -0500

    Added line charts with palceholder data (commented johnny-five code for testing on an environment without functional johnny-five integration)

--- app/js/main.js
+++ app/js/main.js
@@ -7,30 +7,32 @@
 //stdin , short for “standard in”, is the path by which we can pass data into an application. 
 //This is usually text data that the user has typed, but it can also come from another application.
 
-var Readable = require("stream").Readable;  
-var util = require("util");  
-util.inherits(MyStream, Readable);  
-function MyStream(opt) {  
-  Readable.call(this, opt);
-}
-MyStream.prototype._read = function() {};  
-// hook in our stream
-process.__defineGetter__("stdin", function() {  
-  if (process.__stdin) return process.__stdin;
-  process.__stdin = new MyStream();
-  return process.__stdin;
-});
+        // var Readable = require("stream").Readable;  
+        // var util = require("util");  
+        // util.inherits(MyStream, Readable);  
+        // function MyStream(opt) {  
+        // Readable.call(this, opt);
+        // }
+        // MyStream.prototype._read = function() {};  
+        // // hook in our stream
+        // process.__defineGetter__("stdin", function() {  
+        // if (process.__stdin) return process.__stdin;
+        // process.__stdin = new MyStream();
+        // return process.__stdin;
+        // });
 
 // johnny-five
-var johnnyFive = require("johnny-five");                
-var breadboard = new johnnyFive.Board({
-        repl: false                
-});
+        // var johnnyFive = require("johnny-five");                
+        // var breadboard = new johnnyFive.Board({
+        //         repl: false                
+        // });
 
 var humidityDoughnut = document.querySelector("#humidityDoughnut"),  
         lightDoughnut = document.querySelector("#lightDoughnut");
 
-var hydrometerLineChart = document.querySelector("#lineChart");
+var hydrometerLineChart = document.querySelector("#hydrometerLineChart"), 
+    temperatureLineChart = document.querySelector("#temperatureLineChart"),
+    lightLineChart = document.querySelector("#lightLineChart");
 
 var hydrometerCurrReadingCon = document.querySelector("#humidityCurrReading"),
         temperatureCurrReadingCon = document.querySelector("#temperatureCurrReading"), 
@@ -39,19 +41,19 @@ var hydrometerCurrReadingCon = document.querySelector("#humidityCurrReading"),
 var hydrometerCurrReading = 0, temperatureCurrReading = 0, lightCurrReading = 0;
 
 // placeholder line chart data ~
-var data = [1,2,3,2,4,1,2], labels = ["1","2","3","2","4","1","2"];
+var tempData = [20,40,10,20,5,10,23], tempLabels = ["Dec-1","Dec-2","Dec-3","Dec-4","Dec-5","Dec-6","Dec-7"];
 var backgroundColor = ["#24343b", "#f8f8f8"];
 
 // start line chart at zero and remove legend 
-Chart.defaults.scale.ticks.beginAtZero = true
+// Chart.defaults.scale.ticks.beginAtZero = true;
 Chart.defaults.global.legend.display = false;
 
 // generate line/doughnut chart and return chart object
-function generateChart(chartType, dataVal, labels, chartDOM, backgroundColor, borderColor, title){
+function generateChart(chartType, dataVal, chartDOM, backgroundColor, borderColor, title){
     return new Chart(chartDOM, {
         type: chartType, 
             data: {
-                labels: labels,
+                // labels: labels, //~
                 datasets: [
                 { 
                     data: dataVal,
@@ -65,27 +67,24 @@ function generateChart(chartType, dataVal, labels, chartDOM, backgroundColor, bo
                 animateScale: true
             }, 
             title: {
-                        display: true,
-                        text: title
-                    }
-        }, 
-        centerText: {
-            display: true,
-            text: dataVal
+                display: true,
+                text: title, 
+                position: 'bottom'
+            }
         }, 
         // line chart settings
         scales: {
             xAxes: [{
-                gridLines: {
-                    display: true
-                },
                 display: false,
+                gridLines: {
+                    display: false
+                },
             }],
             yAxes: [{
-                gridLines: {
-                    display: true
-                },
                 display: false,
+                gridLines: {
+                    display: false
+                },
             }]
         }
     });
@@ -97,52 +96,72 @@ function updateChartData(chart, updatedVals) {
         chart.update();
 }
 
-function returnPercentage(sensorData) {
-        return (1023 /(sensorData * 100 /1023)).toFixed(2);
+var humidityDoughnutChart = generateChart('pie', [50, 50], humidityDoughnut, backgroundColor, backgroundColor, "Moisture");
+var lightDoughnutChart = generateChart('pie', [50, 50], lightDoughnut, backgroundColor, backgroundColor, "Light");
+
+var hydroLineChartCon = generateChart('line', tempData, hydrometerLineChart, "rgba(255, 255, 255, 0)", backgroundColor[0], "Moisture");
+var tempLineChartCon = generateChart('line', tempData, temperatureLineChart, "rgba(255, 255, 255, 0)", backgroundColor[0], "Temperature");
+var lightLineChartCon = generateChart('line', tempData, lightLineChart, "rgba(255, 255, 255, 0)", backgroundColor[0], "Light");
+
+
+// placeholder data ~~
+temperatureCurrReadingCon.innerHTML = "21°C";
+hydrometerCurrReadingCon.innerHTML = "5%";
+lightCurrReadingCon.innerHTML = "55%";
+// placeholder data ~~
+
+            // breadboard.on("ready", function(){                
+            //     var hydrometer = new johnnyFive.Sensor({
+            //                 pin: "A0",
+            //                 freq: 250,
+            //                 threshold: 2
+            //         }), 
+            //         photoresistor = new johnnyFive.Sensor({
+            //         pin: "A4", 
+            //         freq: 250
+            //     }), 
+            //         temperatureSensor = new johnnyFive.Thermometer({
+            //         controller: "LM35", 
+            //         pin: "A2"
+            //     });         
+
+            //     // update html data reading display and chart  
+            //     hydrometer.on("change", function(){
+            //             // mapping values to 100% using johnny-five API 
+            //             hydrometerCurrReading = this.scaleTo(100, 0);
+            //             hydrometerCurrReadingCon.innerHTML = hydrometerCurrReading + "%";
+            //             updateChartData(humidityDoughnutChart, [hydrometerCurrReading, 100 - hydrometerCurrReading]); 
+            //     });
+
+            //     photoresistor.on("data", function(){
+            //             lightCurrReading = this.scaleTo(100, 0);
+            //             lightCurrReadingCon.innerHTML = lightCurrReading + "%";
+            //             updateChartData(lightDoughnutChart, [lightCurrReading, 100 - lightCurrReading]); 
+            //     });
+
+            //     temperatureSensor.on("change", function(){
+            //         temperatureCurrReadingCon.innerHTML = this.celsius + "°C";
+            //     });
+            // });
+
+// burger menu
+var bmIcon = document.querySelector("#bmIcon");
+var burgerMenuList = document.querySelector("#burgerMenu");
+var mblNavLinks = document.querySelectorAll("#burgerMenuList li a");
+
+function burgerMenu(e) {
+	burgerMenuList.classList.toggle("hidden");
+	bmIcon.classList.toggle("menuExpanded");
 }
 
-var humidityDoughnutChart = generateChart('pie', [0, 100], labels, humidityDoughnut, backgroundColor, backgroundColor, "Moisture");
-var lightDoughnutChart = generateChart('pie', [0, 100], labels, lightDoughnut, backgroundColor, backgroundColor, "Light");
+bmIcon.addEventListener("click", burgerMenu, false);
 
-var lineChartArea = generateChart('line', data, labels, hydrometerLineChart, "rgba(255, 255, 255, 0)", backgroundColor[0], "Moisture");
+// for (var i = 0; i < mblNavLinks.length; i++) {
+// 	mblNavLinks[i].addEventListener("click", smoothScroll, false);
+// 	dsktpNavLinks[i].addEventListener("click", smoothScroll, false);
+// }
 
-breadboard.on("ready", function(){                
-    
-    var hydrometer = new johnnyFive.Sensor({
-                pin: "A0",
-                freq: 250,
-                threshold: 2
-        }), 
-        photoresistor = new johnnyFive.Sensor({
-        pin: "A4", 
-        freq: 250
-    }), 
-        temperatureSensor = new johnnyFive.Thermometer({
-        controller: "LM35", 
-        pin: "A2"
-    });         
-
-    // update html data reading display and chart  
-        hydrometer.on("change", function(){
-                // mapping values to 100% using johnny-five API 
-                hydrometerCurrReading = this.scaleTo(100, 0);
-                hydrometerCurrReadingCon.innerHTML = hydrometerCurrReading + "%";
-                updateChartData(humidityDoughnutChart, [hydrometerCurrReading, 100 - hydrometerCurrReading]); //~
-        });
-
-        photoresistor.on("data", function(){
-                lightCurrReading = this.scaleTo(100, 0);
-                lightCurrReadingCon.innerHTML = lightCurrReading + "%";
-                updateChartData(lightDoughnutChart, [lightCurrReading, 100 - lightCurrReading]); //~
-        });
-
-        temperatureSensor.on("change", function(){
-        // console.log(this.celsius + "C", this.fahrenheit + "F"); //~
-        temperatureCurrReadingCon.innerHTML = this.celsius + "°C";
-        });
-});
-
-// vue
+// // vue
 // var vm = new Vue({ 
 //     el: "#app", 
 //     data: {
@@ -167,20 +186,3 @@ breadboard.on("ready", function(){
 //         }
 //     }
 // });
-
-var bmIcon = document.querySelector("#bmIcon");
-var burgerMenuList = document.querySelector("#burgerMenu");
-var mblNavLinks = document.querySelectorAll("#burgerMenuList li a");
-
-
-function burgerMenu(e) {
-	burgerMenuList.classList.toggle("hidden");
-	bmIcon.classList.toggle("menuExpanded");
-}
-
-bmIcon.addEventListener("click", burgerMenu, false);
-
-for (var i = 0; i < mblNavLinks.length; i++) {
-	mblNavLinks[i].addEventListener("click", smoothScroll, false);
-	dsktpNavLinks[i].addEventListener("click", smoothScroll, false);
-}
